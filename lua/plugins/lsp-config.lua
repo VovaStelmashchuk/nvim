574bf1678f6415fd1f9cd0185b0fb942d425ebad
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2024-05-20 14:32:30 +0300

    add copilot and setup lsp code action

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,9 +8,23 @@ return {
       local coq = require("coq")
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({}))
-      lspconfig.gopls.setup(coq.lsp_ensure_capabilities({}))
+      lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      }))
 
       vim.keymap.set('n', '<space>cf', vim.lsp.buf.format, {})
+      vim.keymap.set('n', '<space>cd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<space>cu', vim.lsp.buf.references, {})
+      --  add auto import action
+      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, {silent = true})
     end
   }
 }
