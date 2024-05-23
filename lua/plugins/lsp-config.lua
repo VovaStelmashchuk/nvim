563d4ed8acc291b7b37fa8f571ea990fa064e055
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2024-05-23 16:21:18 +0300

    small refactor

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,14 +1,13 @@
 return {
   {
-    "ms-jpq/coq_nvim",
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
-      local coq = require("coq")
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({}))
-      lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
+
+      lspconfig.lua_ls.setup({
+      })
+
+      lspconfig.gopls.setup({
         settings = {
           gopls = {
             completeUnimported = true,
@@ -18,13 +17,13 @@ return {
             staticcheck = true,
           },
         },
-      }))
+      })
 
       vim.keymap.set('n', '<space>cf', vim.lsp.buf.format, {})
       vim.keymap.set('n', '<space>cd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<space>cu', vim.lsp.buf.references, {})
       --  add auto import action
-      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, {silent = true})
+      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { silent = true })
     end
   }
 }
