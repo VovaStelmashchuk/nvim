2137920cddc9b5edc4fa3056017103994caa561f
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-12-01 14:11:06 +0200

    clean config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -114,15 +114,4 @@ return {
       })
     end
   },
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim", -- optional
-      "neovim/nvim-lspconfig",       -- optional
-    },
-    opts = {}                        -- your configuration
-  },
 }
--- lua/plugins/path_show.lua
+++ lua/plugins/path_show.lua
@@ -8,7 +8,6 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     opts = {
-      -- configurations go here
     },
   }
 }
