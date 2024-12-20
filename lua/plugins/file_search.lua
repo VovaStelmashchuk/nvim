c4151bedb8b18508d107037daded22c4d451d9b4
Volodymyr Stelmashchuk <volodymyrstelmashchuk@Volodymyr-Stelmashchuk.local>
2024-12-20 09:53:58 +0200

    add buffer pannel

--- lua/plugins/file_search.lua
+++ lua/plugins/file_search.lua
@@ -14,5 +14,6 @@ return {
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>gs', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   end
 }
