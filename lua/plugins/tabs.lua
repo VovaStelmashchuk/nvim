b22eccf86a3cf5f099d57c5145c1cce4fafa3714
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-01-02 09:33:34 +0200

    add tab other close

--- lua/plugins/tabs.lua
+++ lua/plugins/tabs.lua
@@ -13,6 +13,7 @@ return {
       vim.keymap.set('n', '<space>tp', ':BufferPrevious<CR>')
       vim.keymap.set('n', '<space>tg', ':BufferPick<CR>')
       vim.keymap.set('n', '<space>tc', ':BufferClose<CR>')
+      vim.keymap.set('n', '<space>co', ':BufferCloseAllButCurrent<CR>')
     end
   }
 }
